7b09e422a553ad513d04617424edd174160be32e
Andrew Fischer <afischer15@mac.com>
2013-11-01 22:19:13 -0400

    FTP Dump

---
+++ public_html/beta/wp-content/themes/Cutline 1.1-3ColumnRight/_vti_cnf/header.php
@@ -0,0 +1,21 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|29 Jun 2007 19:57:41 -0000
+vti_extenderversion:SR|6.0.2.5516
+vti_author:SR|KLOEPPEL\\brian_g
+vti_modifiedby:SR|IMMUNITY\\Brian Gardner
+vti_timecreated:TR|02 Feb 2007 07:00:26 -0000
+vti_title:SR|<?php if (is_single() || is_page() || is_archive()) { wp_title('',true); } else { bloginfo('name'); echo(' — '); bloginfo('description'); } ?>
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|29 Jun 2007 19:57:41 -0000
+vti_filesize:IR|3220
+vti_cachedtitle:SR|<?php if (is_single() || is_page() || is_archive()) { wp_title('',true); } else { bloginfo('name'); echo(' — '); bloginfo('description'); } ?>
+vti_cachedbodystyle:SR|<body class="custom">
+vti_cachedlinkinfo:VX|Q|< Q|< X|< X|< H|< H|< H|< H|< H|< H|< H|< S|< S|< S|< S|< S|<
+vti_cachedsvcrellinks:VX|DQUS|Cutline\\ 1.1-3ColumnRight/< DQUS|Cutline\\ 1.1-3ColumnRight/< DXUS|Cutline\\ 1.1-3ColumnRight/< DXUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DHUS|Cutline\\ 1.1-3ColumnRight/< DSUS|Cutline\\ 1.1-3ColumnRight/< DSUS|Cutline\\ 1.1-3ColumnRight/< DSUS|Cutline\\ 1.1-3ColumnRight/< DSUS|Cutline\\ 1.1-3ColumnRight/< DSUS|Cutline\\ 1.1-3ColumnRight/<
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_metatags:VR|HTTP-EQUIV=Content-Type <?php\\ bloginfo('html_type');\\ ?>;\\ charset=<?php\\ bloginfo('charset');\\ ?> generator WordPress\\ <?php\\ bloginfo('version');\\ ?>
+vti_charset:SR|windows-1252
+vti_generator:SR|WordPress <?php bloginfo('version'); ?>
